f5edfc49d268f93be2a3aff522622e7fe3f5b2b9
LeurisCastillo <leurisa.delossantos@gmail.com>
2021-07-09 19:19:14 -0400

    estilos agregados

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ app.set('port', (process.env.PORT || 5000));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/public');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
   var env = process.env.APP_ENV;
   if (env == 'staging') {
     var envName = 'staging'
@@ -17,10 +18,10 @@ app.get('/', function(request, response) {
   } else {
     var envName = 'review app'
   }
-  response.render('index.html', { env: envName});
+  response.render('index.html', { env: envName });
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
   console.log("Node app running at localhost:" + app.get('port'));
 });
 
